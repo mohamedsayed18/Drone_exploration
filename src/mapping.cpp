7a3424bc66da531eab004cc82eb5e515dfa7b7ea
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-03-25 15:48:18 +0300

    get if the node is occupied or not

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -1,3 +1,9 @@
+/*
+* Know the size of the the map
+* and if the node is occupied or not
+* getSize() the size of each node is 0.2, which is equal to resolution
+* map->isNodeOccupied(*it)check if the node is occupied or not
+*/
 #include <mapping.h>
 
 static octomap::OcTree* ourmap;
@@ -8,8 +14,12 @@ void setOctomapFromBinaryMsg(const octomap_msgs::Octomap& msg) {
     //iterate over the map
     for (octomap::OcTree::leaf_iterator it = ourmap->begin_leafs();
        it != ourmap->end_leafs(); ++it) {
-           std::cout<<"found a leaf";
+           //std::cout<< "Node center: " << it.getCoordinate();
+           //std::cout<< " value: " << it->getValue()<< "\n";
+           //std::cout<< "size of node " << it.getSize() << "\n";
+           std::cout<< ourmap->isNodeOccupied(*it) <<"\n";
        }
+    std::cout<<"End of tree" << "\n";
 }
 
 int main(int argc, char** argv)

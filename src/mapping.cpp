ac79e5da5b11af267958e02e3f97e59d417e9b72
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-03-26 18:50:08 +0300

    iterate over all nodes

--- src/mapping.cpp
+++ src/mapping.cpp
@@ -3,6 +3,7 @@
 * and if the node is occupied or not
 * getSize() the size of each node is 0.2, which is equal to resolution
 * map->isNodeOccupied(*it)check if the node is occupied or not
+* using get size on the first node we get the dimensions of the map
 */
 #include <mapping.h>
 
@@ -11,14 +12,27 @@ static octomap::OcTree* ourmap;
 void setOctomapFromBinaryMsg(const octomap_msgs::Octomap& msg) {
     ourmap = dynamic_cast<octomap::OcTree*>(octomap_msgs::binaryMsgToMap(msg));
     
-    //iterate over the map
+    //iterate over leaf nodes
+    /*
     for (octomap::OcTree::leaf_iterator it = ourmap->begin_leafs();
        it != ourmap->end_leafs(); ++it) {
            //std::cout<< "Node center: " << it.getCoordinate();
            //std::cout<< " value: " << it->getValue()<< "\n";
            //std::cout<< "size of node " << it.getSize() << "\n";
-           std::cout<< ourmap->isNodeOccupied(*it) <<"\n";
+           //std::cout<< ourmap->isNodeOccupied(*it) <<"\n";
+           std::cout<< (ourmap->getRoot()).getSize();
+           break;
        }
+    */
+    //iterate over all nodes
+    octomap::OcTreeNode * node = ourmap->getRoot();
+
+    for (octomap::OcTree::tree_iterator it = ourmap->begin_tree(); 
+        it != ourmap->end_tree(); ++it)
+    {
+        std::cout<< it.getSize() <<;
+    }
+    
     std::cout<<"End of tree" << "\n";
 }
 
